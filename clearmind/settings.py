418bb3be4c4a90059d701f93ca0a37f61032815f
AlonaTR <alionatrubchaninova443@gmail.com>
2024-06-28 15:43:18 +0200

    truncate allowed hosts

--- clearmind/settings.py
+++ clearmind/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$cd@^95(qmuvsfq4rxzp1#9^-wvdzz&g7i#j16u8n30pmy5f=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
